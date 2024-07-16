615c77e37a20a1e9cc9dd2c44c063c2f031be1d2
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2024-07-16 11:42:46 -0500

    [add] - Swagger Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  const configSwagger = new DocumentBuilder()
+    .setTitle('API SmartFooloose')
+    .setDescription('API for the SmartFootloose backend')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
 
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('/api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
--- src/modules/client/dto/cab-client.dto.ts
+++ src/modules/client/dto/cab-client.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CabClientDto {
+  @ApiProperty({ description: 'Name Client / City' })
   @IsNotEmpty()
   @IsString()
   cliente: String;
